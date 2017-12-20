e330f9941fd737e9693cd6b8a7007342a2195fd7
calebredd <caleb.redd@yahoo.com>
2017-12-19 19:09:03 -0700

    Select all

--- googleHomePage.js
+++ googleHomePage.js
@@ -13,3 +13,13 @@ function google(){
     if(window.confirm("This is not a real Google Search Page click \"ok\" to go to Google's homepage."))
         {window.location.href="https://www.google.com";}
 };
+let luck=document.querySelector(".lucky");
+luck.addEventListener("mouseover",function(e){
+    luck.innerHTML="I'm Feeling Risky";
+    });
+luck.addEventListener("mouseleave",function(e){
+    luck.innerHTML="I'm Feeling Lucky";
+    });
+search.addEventListener("click",function(e){
+    search.select();
+});
